b28a1550be35a8457247152e39ac796d8fc3842d
R4V3NSH4D0W <lenishmagar@gmail.com>
2025-02-02 17:23:03 +0545

    Minor changes

--- src/app/(root)/login/page.tsx
+++ src/app/(root)/login/page.tsx
@@ -65,12 +65,6 @@ export default function LoginPage() {
     }
   };
 
-  useEffect(() => {
-    if (Cookie.get("token")) {
-      router.push("/");
-    }
-  }, [router]);
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <Card className="w-full max-w-md p-6 shadow-xl">
