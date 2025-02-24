24d645f17b08a366bea029bec4b7f378fe98f340
R4V3NSH4D0W <lenishmagar@gmail.com>
2025-02-24 19:44:21 +0545

    AG-17: Minor UI changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -101,7 +101,7 @@ function DashBoard() {
             <TabsTrigger value="all">All</TabsTrigger>
             <TabsTrigger value="active">Active</TabsTrigger>
             <TabsTrigger value="inactive">Inactive</TabsTrigger>
-            <TabsTrigger value="draft">Draft</TabsTrigger>
+            {/* <TabsTrigger value="draft">Draft</TabsTrigger> */}
             <TabsTrigger value="archived" className="hidden sm:flex">
               Archived
             </TabsTrigger>
